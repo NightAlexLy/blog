6ebebdbaa65b498597e9b52e08d3e33bb466b862
LuisYang <night_ly@live.com>
2018-02-08 19:57:16 +0800

    update doc20180208

--- auto_update.bat
+++ auto_update.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set "currentYMD=%date:~,4%%date:~5,2%%date:~8,2%"
-git pull origin master
-git status
-git add .
-git commit -m "update doc"%currentYMD%
-git push origin master
-pause >> d:/logs/update.log
+set "currentYMD=%date:~,4%%date:~5,2%%date:~8,2%" 
+git pull origin master  >> d:/logs/update.log
+git status  >> d:/logs/update.log
+git add .  >> d:/logs/update.log
+git commit -m "update doc"%currentYMD% >> d:/logs/update.log
+git push origin master >> d:/logs/update.log
+pause 
